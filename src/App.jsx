4048e8458edb52f3c769397f484fd36e03d64809
adikrnwn171 <adikrnwn171@gmail.com>
2024-07-23 22:43:12 +0700

    admin-lte

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Test from "./pages/test";
+import Halo from "./pages/halo";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Test />} />
+          <Route path="/" element={<Halo />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </BrowserRouter>
     </>
